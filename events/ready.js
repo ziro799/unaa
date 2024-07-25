88934dcebc4fb14caa940c3d1af1f5ed8c2305ae
ziro799 <84936645+ziro799@users.noreply.github.com>
2024-07-25 12:29:10 +0700

    unaaa

--- events/ready.js
+++ events/ready.js
@@ -44,8 +44,8 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Update v3.6`, 
-  type: ActivityType.Watching }), 10000);
+  name: `Unaa V3`, 
+  type: ActivityType.Listening }), 10000);
 client.errorLog = config.errorLog
 } else {
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
