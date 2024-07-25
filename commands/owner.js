f8627a74e314dbfba3bfa3a8db56ff82fc2dd63c
ziro799 <84936645+ziro799@users.noreply.github.com>
2024-07-25 12:25:16 +0700

    anjay mabar

--- commands/owner.js
+++ commands/owner.js
@@ -29,17 +29,17 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
-      const youtubeLink = 'https://discord.gg/xQF9f9yUEM';
-      const InstagramLink = 'https://www.instagram.com/rtxxgg/';
+      const youtubeLink = 'https://discord.gg/myznn';
+      const InstagramLink = 'https://www.instagram.com/nopallsution/';
       const { EmbedBuilder } = require('discord.js')
         const embed = new EmbedBuilder()
             .setColor('#da2a41')
             .setAuthor({
           name: 'Owner',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/4mKkjBFMeS'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [RTX GAMING](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n\n ▶️ Myself Miles aka Rio . I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n Discord : [ Miles ] ❤️ (${youtubeLink})\n Instagram : 💙 [Miles](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
